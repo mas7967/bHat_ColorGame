82eeb81f5ce8bca08e2edd5b96a93fbd2ed8e71c
Mark Schillaci <mark.schillaci@gmail.com>
2015-07-02 23:22:32 -0400

    Initial repo setup

---
+++ app/src/main/java/com/bhat/colorgame/ColorGameActivityFragment.java
@@ -0,0 +1,23 @@
+package com.bhat.colorgame;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ColorGameActivityFragment extends Fragment {
+
+    public ColorGameActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_color_game, container, false);
+    }
+}
